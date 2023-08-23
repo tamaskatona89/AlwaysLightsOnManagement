5a665e4c6aa7736d427ab22ab3bae7c135e3651a
katonatomi <katonatomi@msn.com>
2023-08-23 17:08:29 +0200

    Project started with DLL

---
+++ AlwaysLightsOnManagement/AlwaysLightsOnManagement/ReportedIssues.cs
@@ -0,0 +1,44 @@
+﻿using System;
+/**
+* author: KATONA TAMAS - katonatomi@msn.com
+* 2023.
+*/
+namespace AlwaysLightsOnManagement
+{
+    public class ReportedIssues
+    {
+        public int Issue_ID { get; set; }
+        public int ZIP_CODE { get; set; }
+        public string Address { get; set; }
+        public DateTime Reported_DateTime { get; set; } = DateTime.Now;
+        public bool Is_Fixed { get; set; }
+
+        /// <summary>
+        /// Create 1 Issue
+        /// </summary>
+        /// <param name="zipCode">zipCode</param>
+        /// <param name="address">address</param>
+        public ReportedIssues(int zipCode, string address)
+        {
+            ZIP_CODE = zipCode;
+            Address = address;
+            Reported_DateTime = DateTime.Now;
+            Is_Fixed = false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
