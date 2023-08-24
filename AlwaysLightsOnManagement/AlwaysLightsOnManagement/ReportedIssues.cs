35dc5afa53a150d3db04086aa1d73b3f4d7d8b03
katonatomi <katonatomi@msn.com>
2023-08-24 14:52:50 +0200

    Revert "Database Table DataModels created"
    This reverts commit 1ce62eb113bf27b197d1bc9953b2556db289e6e0.

--- AlwaysLightsOnManagement/AlwaysLightsOnManagement/ReportedIssues.cs
+++ AlwaysLightsOnManagement/AlwaysLightsOnManagement/ReportedIssues.cs
@@ -7,11 +7,11 @@ namespace AlwaysLightsOnManagement
 {
     public class ReportedIssues
     {
-        public int IssueID { get; set; }
-        public int ZIPCODE { get; set; }
+        public int Issue_ID { get; set; }
+        public int ZIP_CODE { get; set; }
         public string Address { get; set; }
-        public DateTime ReportedDateTime { get; set; } = DateTime.Now;
-        public bool IsFixed { get; set; }
+        public DateTime Reported_DateTime { get; set; } = DateTime.Now;
+        public bool Is_Fixed { get; set; }
 
         /// <summary>
         /// Create 1 Issue
@@ -20,10 +20,10 @@ namespace AlwaysLightsOnManagement
         /// <param name="address">address</param>
         public ReportedIssues(int zipCode, string address)
         {
-            ZIPCODE = zipCode;
+            ZIP_CODE = zipCode;
             Address = address;
-            ReportedDateTime = DateTime.Now;
-            IsFixed = false;
+            Reported_DateTime = DateTime.Now;
+            Is_Fixed = false;
         }
 
         public override bool Equals(object obj)
