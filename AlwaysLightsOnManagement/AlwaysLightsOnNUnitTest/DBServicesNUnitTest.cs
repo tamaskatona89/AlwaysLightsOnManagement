872e0b6565b3c5755acdb78d19a04d128c9e78d1
katonatomi <katonatomi@msn.com>
2023-09-05 10:05:01 +0200

    NUnit All Tests Passed

--- AlwaysLightsOnManagement/AlwaysLightsOnNUnitTest/DBServicesNUnitTest.cs
+++ AlwaysLightsOnManagement/AlwaysLightsOnNUnitTest/DBServicesNUnitTest.cs
@@ -87,5 +87,105 @@ namespace AlwaysLightsOnNUnitTest
             //CHECK ID-s BEFORE AND AFTER DELETION
             Assert.That(lastWorkerId2, Is.Not.EqualTo(lastWorkerId));
         }
+
+        [Test]
+        public void Test_GetReportedIssuesByZIPCode()
+        {
+            List<ReportedIssue>? resultList = DBServicesInstance.GetReportedIssuesByZIPCode(4400);
+            Assert.IsNotNull(resultList);
+            resultList = DBServicesInstance.GetReportedIssuesByZIPCode(3700);
+            Assert.IsNotNull(resultList);
+        }
+
+        [Test]
+        public void Test_GetBudapestReportedOPENIssuesByDistrict()
+        {
+            List<ReportedIssue>? resultList = DBServicesInstance.GetBudapestReportedOPENIssuesByDistrict(106);
+            Assert.IsNotNull(resultList);
+        }
+
+        [Test]
+        public void Test_GetReportedIssuesOlderThan()
+        {
+            List<ReportedIssue>? resultList = DBServicesInstance.GetReportedIssuesOlderThan(1);
+            Assert.IsNotNull(resultList);
+        }
+
+        [Test]
+        public void Test_AddFinishedWorkToWorkList_AndReadBackData()
+        {
+            DBServicesInstance.AddFinishedWorkToWorkList(51, 1, 1);
+            DBServicesInstance.SaveChanges();
+
+            //GET DATA BACK
+            WorkList lastWork = DBServicesInstance.WorkLists.OrderBy(wl => wl.WorkListId).Last();
+            Assert.That(lastWork.IssueId, Is.EqualTo(51));
+
+        }
+
+        [Test]
+        public void Test_ReportedIssuesQuery_byNumber_Switch()
+        {
+            List<ReportedIssue>? resultList = DBServicesInstance.ReportedIssuesQuery_byNumber_Switch(106);
+            Assert.IsNotNull(resultList);
+
+        }
+
+        [Test]
+        public void Test_GetWorkListByTime()
+        {
+            List<ExportableWorkList>? resultList = DBServicesInstance.GetWorkListByTime(2023, 8);
+            Assert.IsNotNull(resultList);
+
+        }
+
+        [Test]
+        public void Test_GetWorkListByMonth_GroupByWorkTypes()
+        {
+            List<ExportableWorkList>? resultList = DBServicesInstance.GetWorkListByMonth_GroupByWorkTypes(2023, 8);
+            Assert.IsNotNull(resultList);
+
+        }
+
+        [Test]
+        public void Test_GetWorkListByWorkerIDAndTime()
+        {
+            List<ExportableWorkList>? resultList = DBServicesInstance.GetWorkListByWorkerIDAndTime(6, 2023, 8);
+            Assert.IsNotNull(resultList);
+
+        }
+
+        [Test]
+        public void Test_GetWorkListByWorkTypeIDAndTime()
+        {
+            List<ExportableWorkList>? resultList = DBServicesInstance.GetWorkListByWorkTypeIDAndTime(1, 2023, 8);
+            Assert.IsNotNull(resultList);
+
+        }
+
+        [Test]
+        public void Test_GetWorkers()
+        {
+            List<Worker>? resultList = DBServicesInstance.GetWorkers();
+            Assert.IsNotNull(resultList);
+
+        }
+
+        [Test]
+        public void Test_CreateXML()
+        {
+            List<ExportableWorkList> resultList = new List<ExportableWorkList>();
+            resultList.Add(new ExportableWorkList { WorkListID = 1, ReportedIssue = "ReportedIssue", WorkType = "WorkType", Worker = "Worker", FixingDateTime = DateTime.Now });
+            DBServicesInstance.CreateXML("file.xml", resultList);
+
+            if (File.Exists("file.xml"))
+            {
+                Assert.IsNotNull(resultList);
+            }
+
+
+        }
+
+
     }
 }
